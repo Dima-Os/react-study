9d58d786b361639ff96108b74c80b784552b0bb0
Dima Osipov <osipovdima1989@gmail.com>
2021-01-08 14:19:56 +0200

    were added form

--- src/App.jsx
+++ src/App.jsx
@@ -10,16 +10,34 @@ import Todos from './components/Todos';
 import initialTodos from './initialTodos.json';
 
 class App extends Component {
-  state = { todos: initialTodos };
+  state = {
+    todos: initialTodos,
+    name: '',
+    tag: '',
+  };
   onClick = id => {
     console.log(id);
     this.setState(prevState => ({
       todos: prevState.todos.filter(todo => todo.id !== id),
     }));
   };
+  handleInputChange = e => {
+    const { name, value } = e.currentTarget;
+    this.setState({ [name]: value });
+  };
   render() {
     return (
       <Container>
+        <form>
+          <label>
+            Enter your name:
+            <input onChange={this.handleInputChange} name="name" type="text" />
+          </label>
+          <label>
+            Enter your tag:
+            <input onChange={this.handleInputChange} name="tag" type="text" />
+          </label>
+        </form>
         <Todos todos={this.state.todos} onClick={this.onClick} />
         {/* <ColorPicker options={colorPickerOptions}/>
       <Dropdown />
