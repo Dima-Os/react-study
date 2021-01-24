279ebd09479754910f8a29ae8af0087c5878b3ab
Dima_Os <osipovdima1989@gmail.com>
2021-01-24 13:06:08 +0200

    were added iconBtns

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import s from './Todo.module.css';
+import { ReactComponent as DeleteBtn } from '../../images/delete.svg';
+import IconBtn from '../IconBtn';
 
 const Todo = ({ text, onClick, id, completed, onInputChange }) => {
   return (
     <>
       <p className={s.text}>{text}</p>
-      <button type="button" onClick={() => onClick(id)}>
-        Delete
-      </button>
+      <IconBtn onClick={() => onClick(id)} aria-label="Добавить">
+        <DeleteBtn width="40" height="40" />
+      </IconBtn>
       <label>
         <input
           type="checkbox"
